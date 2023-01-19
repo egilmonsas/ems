7a85cb95f451296cbcf0a916530285321722a3a0
Vasse <egilmonsas@gmai.com>
2023-01-19 21:01:22 +0100

    changed the api implementation to incorporate the dev branch of emsdesign

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,8 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-use emsdesign::crs::standard::{CrsLib, PresetCrs, PRESETS};
 use emsdesign::crs::CrossSection;
+use emsdesign::crs::{CrsLib, PRESETS};
 use emsdesign::erc::NSEN_1993::BuckleCurve;
 use emsdesign::mat::steel::{Steel, Variant};
 use emsdesign::mat::Material;
@@ -29,28 +29,24 @@ fn main() {
 #[tauri::command]
 fn get_area(crstype: &str, name: &str) -> Result<Value, String> {
     let preset = PRESETS::get(crstype).ok_or_else(|| "Could not get preset".to_owned())?;
-    let df = CrsLib::new(&preset).map_err(|e| format!("{}", e))?;
-    let crs = PresetCrs::new(name, &df).map_err(|e| format!("{}", e))?;
+    let crs = CrsLib::get(&preset, name);
 
     Ok(crs.json())
 }
 #[tauri::command]
 fn get_capacity(crstype: &str, name: &str, material: &str) -> Result<Value, String> {
     let preset = PRESETS::get(crstype).ok_or_else(|| "Could not get preset".to_owned())?;
-    let df = CrsLib::new(&preset).map_err(|e| format!("{}", e))?;
-    let crs = PresetCrs::new(name, &df).map_err(|e| format!("{}", e))?;
+    let crs = CrsLib::get(&preset, name);
     let matvariant = Variant::get(material).ok_or_else(|| "Could not get material".to_owned())?;
     let mat = Steel::from(&matvariant);
-    let cmb = ColumnBeam::new(Box::new(crs), mat);
+    let cmb = ColumnBeam::new(crs, mat);
     Ok(cmb.json())
 }
 
 #[tauri::command]
 fn get_section_names(crstype: &str) -> Result<Value, String> {
     let preset = PRESETS::get(crstype).ok_or_else(|| "Could not get preset".to_owned())?;
-    let df = CrsLib::new(&preset).map_err(|e| format!("{}", e))?;
-    let sections = df.sections().map_err(|e| format!("{}", e))?;
-    Ok(json!(sections))
+    Ok(json!(CrsLib::sections(&preset)))
 }
 #[tauri::command]
 fn get_steel_variants() -> Value {
@@ -85,11 +81,10 @@ fn get_buckle_curve(
     let limitstate =
         LimitStateType::get(limitstate).ok_or_else(|| "Could not get limitstate".to_owned())?;
     let preset = PRESETS::get(crstype).ok_or_else(|| "Could not get preset".to_owned())?;
-    let df = CrsLib::new(&preset).map_err(|e| format!("{}", e))?;
-    let crs = PresetCrs::new(name, &df).map_err(|e| format!("{}", e))?;
+    let crs = CrsLib::get(&preset, name);
     let matvariant = Variant::get(material).ok_or_else(|| "Could not get material".to_owned())?;
     let mat = Steel::from(&matvariant);
-    let cmb = ColumnBeam::new(Box::new(crs), mat);
+    let cmb = ColumnBeam::new(crs, mat);
 
     let buckle_curve_y =
         BuckleCurve::get(curve_y).ok_or_else(|| "Could not get buckle curve y".to_owned())?;
@@ -123,77 +118,20 @@ mod tests {
 
     #[test]
     fn lib_import_works() {
-        let df = CrsLib::new(&PRESETS::HEB).expect("Could not build df");
-        let crs = PresetCrs::new("HEB 100", &df).expect("Could not build crs");
+        let crs = CrsLib::get(&PRESETS::HEB, "HEB 100");
         dbg!(crs.json());
     }
 
     #[test]
     fn sectionnamesworks() {
-        let df = CrsLib::new(&PRESETS::HEB).expect("Could not build df");
-        dbg!(df.sections().expect("Could get sections"));
-        dbg!(json!(df.sections().expect("Could get sections")));
+        dbg!(CrsLib::sections(&PRESETS::HEB));
     }
     #[test]
     fn get_capacity() {
-        let df = CrsLib::new(&PRESETS::get("HEB").expect("Could not get preset"))
-            .expect("Could not build df");
-
-        let crs = PresetCrs::new("HEB 100", &df).expect("Could not build crs");
+        let crs = CrsLib::get(&PRESETS::HEB, "HEB 100");
         let matvariant = Variant::get("S355").unwrap();
         let mat = Steel::from(&matvariant);
-
-        let cmb = ColumnBeam::new(Box::new(crs), mat);
+        let cmb = ColumnBeam::new(crs, mat);
         dbg!(cmb.json());
-    }
-
-    #[test]
-    fn get_buckle_curve() {
-        #[derive(Serialize, Deserialize)]
-        #[allow(non_snake_case)]
-        struct Capacity {
-            L_k: f64,
-            N_eu_y: f64,
-            N_eu_z: f64,
-            N_rd_y: f64,
-            N_rd_z: f64,
-            N_pl: f64,
-        }
-
-        let df = CrsLib::new(&PRESETS::get("HEB").expect("Could not get preset"))
-            .expect("Could not build df");
-
-        let crs = PresetCrs::new("HEB 100", &df).expect("Could not build crs");
-        let matvariant = Variant::get("S355").unwrap();
-        let mat = Steel::from(&matvariant);
-
-        let cmb = ColumnBeam::new(Box::new(crs), mat);
-
-        let mut l = 1000.0;
-        let l_max = 20000.0; //mm
-        let dl = 1000.0; //mm
-
-        let mut out: Vec<Capacity> = Vec::new();
-        let buckle_curve_y = BuckleCurve::get("A").expect("Could not get buckle curve y");
-        let buckle_curve_z = BuckleCurve::get("C").expect("Could not get buckle curve z");
-
-        while l <= l_max {
-            l += dl;
-            out.push(Capacity {
-                L_k: l,
-                N_pl: cmb.N_pl(&LimitStateType::K),
-                N_eu_y: cmb.euler_load(l, Axis::Y),
-                N_eu_z: cmb.euler_load(l, Axis::Z),
-                N_rd_y: cmb.buckle_cap(l, Axis::Y, &buckle_curve_y, &LimitStateType::D),
-                N_rd_z: cmb.buckle_cap(l, Axis::Z, &buckle_curve_z, &LimitStateType::D),
-            });
-        }
-        let json = json![out];
-        use std::fs::File;
-        serde_json::to_writer(
-            &File::create("data.json").expect("Couldnt create file"),
-            &json,
-        )
-        .expect("Couldnt write file");
     }
 }
